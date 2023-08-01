f3a96d40a9494e6deec06fe2323cebf20fffcab2
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-08-01 15:45:27 +0530

    chore: added nav bar and list styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import CharacterBox from './components/CharacterBox.svelte';
+  import CharacterBox from "./components/CharacterBox.svelte";
 
   import { charactersStore } from "./stores.js";
   let characters;
@@ -31,52 +31,126 @@
     switch (selectedCharacter) {
       case "Ryuk":
         if (characters[0].flag == true) {
-          displayCharacters = displayCharacters.filter((character) => character.name !== "Ryuk");
+          displayCharacters = displayCharacters.filter(
+            (character) => character.name !== "Ryuk"
+          );
         }
         break;
       case "Keanu Reeves":
         if (characters[1].flag == true) {
-          displayCharacters = displayCharacters.filter((character) => character.name !== "Keanu Reeves");
+          displayCharacters = displayCharacters.filter(
+            (character) => character.name !== "Keanu Reeves"
+          );
         }
         break;
       case "Kenny":
         if (characters[2].flag == true) {
-          displayCharacters = displayCharacters.filter((character) => character.name !== "Kenny");
+          displayCharacters = displayCharacters.filter(
+            (character) => character.name !== "Kenny"
+          );
         }
         break;
       case "Ferb":
         if (characters[3].flag == true) {
-          displayCharacters = displayCharacters.filter((character) => character.name !== "Ferb");
+          displayCharacters = displayCharacters.filter(
+            (character) => character.name !== "Ferb"
+          );
         }
         break;
       case "Spider-Man":
         if (characters[4].flag == true) {
-          displayCharacters = displayCharacters.filter((character) => character.name !== "Spider-Man");
+          displayCharacters = displayCharacters.filter(
+            (character) => character.name !== "Spider-Man"
+          );
         }
         break;
-        
     }
   }
 </script>
 
 <main on:click={handleClick}>
-  <CharacterBox top={2920} left={240} width={45} height={95} onCharacterClick={() => (characters[0].flag = true)} />
-  <CharacterBox top={3810} left={590} width={70} height={65} onCharacterClick={() => (characters[1].flag = true)} />
-  <CharacterBox top={4020} left={970} width={130} height={180} onCharacterClick={() => (characters[1].flag = true)} />
-  <CharacterBox top={4830} left={880} width={50} height={40} onCharacterClick={() => (characters[2].flag = true)} />
-  <CharacterBox top={4990} left={1460} width={10} height={40} onCharacterClick={() => (characters[3].flag = true)} />
-  <CharacterBox top={5915} left={950} width={60} height={60} onCharacterClick={() => (characters[4].flag = true)} />
+  <nav>
+    <ul class="navbar">
+      {#each displayCharacters as character}
+        <li>{character.name}</li>
+        <img src={character.path} alt="nah" />
+      {/each}
+    </ul>
+  </nav>
+
+  <CharacterBox
+    top={2920}
+    left={240}
+    width={45}
+    height={95}
+    onCharacterClick={() => (characters[0].flag = true)}
+  />
+  <CharacterBox
+    top={3810}
+    left={590}
+    width={70}
+    height={65}
+    onCharacterClick={() => (characters[1].flag = true)}
+  />
+  <CharacterBox
+    top={4020}
+    left={970}
+    width={130}
+    height={180}
+    onCharacterClick={() => (characters[1].flag = true)}
+  />
+  <CharacterBox
+    top={4830}
+    left={880}
+    width={50}
+    height={40}
+    onCharacterClick={() => (characters[2].flag = true)}
+  />
+  <CharacterBox
+    top={4990}
+    left={1460}
+    width={10}
+    height={40}
+    onCharacterClick={() => (characters[3].flag = true)}
+  />
+  <CharacterBox
+    top={5915}
+    left={950}
+    width={60}
+    height={60}
+    onCharacterClick={() => (characters[4].flag = true)}
+  />
 
   {#if show == true}
-    <ul style="position: absolute; top: {mouseY}px; left: {mouseX}px;">
+    <ul class="character-list" style=" top: {mouseY}px; left: {mouseX}px;">
       {#each displayCharacters as character}
-        <li on:click={() => selectCharacter(character)}>{character.name}</li>
+        <li class="character-item" on:click={() => selectCharacter(character)}>
+          {character.name}
+        </li>
       {/each}
     </ul>
   {/if}
 </main>
 
 <style>
+  nav {
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 8px;
+  }
+
+  .navbar {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    gap: 10%;
+    font-size: 18px;
+  }
+
+  .navbar>img{
+   height: 20px;
+  }
+
+
   img {
     margin: 0;
     padding: 0;
@@ -91,5 +165,18 @@
     height: 6400px;
     background-image: url("/background.jpg");
     background-size: contain;
+  }
+
+  .character-list {
+    position: absolute;
+    padding: 8px;
+    background-color: black;
+    border-radius: 4px;
+    list-style: none;
+  }
+  .character-item {
+    font-size: 16px;
+    padding: 6px 12px;
+    cursor: pointer;
   }
 </style>
--- src/stores.js
+++ src/stores.js
@@ -1,9 +1,9 @@
 import { writable } from 'svelte/store';
 
 export const charactersStore = writable([
-  { name: "Ryuk", flag: false },
-  { name: "Keanu Reeves", flag: false },
-  { name: "Kenny", flag: false },
-  { name: "Ferb", flag: false },
-  { name: "Spider-Man", flag: false },
+  { name: "Ryuk", flag: false, path: "./src/assets/images/ryuk.jpg" },
+  { name: "Keanu Reeves", flag: false, path: "./src/assets/images/keanu-reeves.jpg" },
+  { name: "Kenny", flag: false, path: "./src/assets/images/kenny.png" },
+  { name: "Ferb", flag: false, path: "./src/assets/images/ferb.jpg" },
+  { name: "Spider-Man", flag: false, path: "./src/assets/images/spidey.png" },
 ]);
